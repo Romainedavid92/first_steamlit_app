b43900803d86efed68451c284956202020f0bed7
David Romaine <53546152+Romainedavid92@users.noreply.github.com>
2022-11-17 15:12:27 -0600

    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-fruits_to_show = myfruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # fruit dataframe 
 streamlit.dataframe(fruits_to_show) # pandas df turned to streamlist df
