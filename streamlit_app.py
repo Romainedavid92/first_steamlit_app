6d5fea6b5c50c427f2622b97202628ccd97925e2
David Romaine <53546152+Romainedavid92@users.noreply.github.com>
2022-11-17 15:10:52 -0600

    make table dynamic

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 #specials Header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = myfruit_list.loc[fruits_selected]
 
 # fruit dataframe 
-streamlit.dataframe(my_fruit_list) # pandas df turned to streamlist df
+streamlit.dataframe(fruits_to_show) # pandas df turned to streamlist df
