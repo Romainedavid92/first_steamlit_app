68085a9ac339b748b76e3d8c382fe67ae5773fc4
David Romaine <53546152+Romainedavid92@users.noreply.github.com>
2022-11-17 14:55:03 -0600

    added emojis and specials header text

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 # body
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+#specials Header
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
