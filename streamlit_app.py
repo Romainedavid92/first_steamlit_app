21700cea39152c0e11985a378e2fa8197555efd9
David Romaine <53546152+Romainedavid92@users.noreply.github.com>
2022-11-17 14:51:05 -0600

    added text and body to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
+# title
 streamlit.title('My Parents New Healthy Diner')
+
+# body
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
