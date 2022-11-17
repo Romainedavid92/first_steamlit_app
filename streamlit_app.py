e0fd4e1b8a6ead169828217dfaa63647af93d135
David Romaine <53546152+Romainedavid92@users.noreply.github.com>
2022-11-17 15:09:23 -0600

    example fruit text

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 #specials Header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 # fruit dataframe 
 streamlit.dataframe(my_fruit_list) # pandas df turned to streamlist df
