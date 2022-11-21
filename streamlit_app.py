a1a8214fc33e4c74ac61a649f8f8b7a6a9fb5ac7
David Romaine <53546152+Romainedavid92@users.noreply.github.com>
2022-11-21 10:07:22 -0600

    allow user input for fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,6 @@ import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruit_variable = 'Kiwi'
-
-# api request
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_variable}")
-
 # title
 streamlit.title('My Parents New Healthy Diner')
 
@@ -32,6 +27,12 @@ streamlit.dataframe(fruits_to_show) # pandas df turned to streamlist df
 
 
 streamlit.header('Fruityvice Fruit Advice!')
+
+fruit_variable = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered', fruit_variable)
+
+# api request
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_variable}")
 
 fruity_vice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruity_vice_normalized)
